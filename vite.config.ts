43f265efc2f1afc4c7067a9344167fc1ebb791b1
Lucas-Napoli <lucasnapolipacheco@gmail.com>
2025-01-27 20:57:26 -0300

    configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NousDojoLandingPage/', // Substitua pelo nome do seu repositório no GitHub
+  base: '/NousDojoLandingPage', // Substitua pelo nome do seu repositório no GitHub
 });
